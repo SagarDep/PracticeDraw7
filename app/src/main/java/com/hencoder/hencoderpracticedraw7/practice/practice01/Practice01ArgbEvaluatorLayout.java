548d4afda2d9dc27436c70be226134a499c7f786
Bakumon <bakumon@aliyun.com>
2017-12-27 15:07:29 +0800

    完成练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice01/Practice01ArgbEvaluatorLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice01/Practice01ArgbEvaluatorLayout.java
@@ -1,8 +1,11 @@
 package com.hencoder.hencoderpracticedraw7.practice.practice01;
 
+import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
@@ -38,9 +41,16 @@ public class Practice01ArgbEvaluatorLayout extends RelativeLayout {
             public void onClick(View v) {
                 ObjectAnimator animator = ObjectAnimator.ofInt(view, "color", 0xffff0000, 0xff00ff00);
                 // 在这里使用 ObjectAnimator.setEvaluator() 来设置 ArgbEvaluator，修复闪烁问题
+                animator.setEvaluator(new ArgbEvaluator());
                 animator.setInterpolator(new LinearInterpolator());
                 animator.setDuration(2000);
                 animator.start();
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                        Log.e("mafei", "onAnimationUpdate: " + valueAnimator.getAnimatedValue());
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
@@ -48,11 +48,13 @@ public class Practice03OfObjectLayout extends RelativeLayout {
     }
 
     private class PointFEvaluator implements TypeEvaluator<PointF> {
-
+        PointF newPoint = new PointF();
         // 重写 evaluate() 方法，让 PointF 可以作为属性来做动画
         @Override
         public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
-            return startValue;
+            newPoint.x = (endValue.x - startValue.x) * fraction + startValue.x;
+            newPoint.y = (endValue.y - startValue.y) * fraction + startValue.y;
+            return newPoint;
         }
     }
 }
